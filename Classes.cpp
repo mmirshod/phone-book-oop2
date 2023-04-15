b2b40533f6e845f7795c5ecceeb78bd6c2cdd135
Mirshod Maxsudov <mirshodmahsudov@gmail.com>
2023-04-15 23:31:38 +0500

    initial commit

---
+++ Classes.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ENVY on 4/15/2023.
+//
+
+#include "Classes.h"
---
+++ Classes.h
@@ -0,0 +1,22 @@
+//
+// Created by ENVY on 4/15/2023.
+//
+
+#ifndef PHONE_BOOK_CLASSES_H
+#define PHONE_BOOK_CLASSES_H
+
+
+class Person {
+private:
+public:
+protected:
+};
+
+class Contact {
+private:
+protected:
+public:
+};
+
+
+#endif //PHONE_BOOK_CLASSES_H
